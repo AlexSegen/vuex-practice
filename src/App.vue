15fcecfd8f9c32adc217aec5207525616143bbe9
Alejandro Vivas <alejandrojv17@gmail.com>
2018-11-07 10:13:09 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <h1>Todos</h1>
+    <ul>
+    <li>Hola</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  max-width: 400px;
+  margin: 30px auto;
+}
+</style>
